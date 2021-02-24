3855c98a92eece6d909d02f4480fc4fa3f43b891
sally-king <sally@kingcastle.org>
2021-02-24 13:40:47 -0800

    Initial Commit

---
+++ Shared/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  Shared
+//
+//  Created by Lyndell King on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
